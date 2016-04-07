b1724e8d5b7926d140332cfbb863dc83c1f0d736
Jan Obdrzalek <jan.obdrzalek@kompitech.com>
2016-04-07 13:34:57 +0200

    Merging release back

--- tests.sh
+++ tests.sh
@@ -1,2 +1,3 @@
 if [ -f new.data ] ; then echo f_1 OK ; else echo f_1 BAD ; fi
 if [ -f new.data ] ; then echo f_2 OK ; else echo f_2 BAD ; fi
+if [ -f version.txt ] ; then echo release 1.0 OK ; else echo release 1.0 BAD ; fi
