a20f33aa71069f8d878abece1cef6664dc712edd
Jan Obdrzalek <jan.obdrzalek@kompitech.com>
2016-04-07 13:34:57 +0200

    f_4 included

--- tests.sh
+++ tests.sh
@@ -1,3 +1,4 @@
 if [ -f new.data ] ; then echo f_1 OK ; else echo f_1 BAD ; fi
 if [ -f new.data ] ; then echo f_2 OK ; else echo f_2 BAD ; fi
 if [ -f version.txt ] ; then echo release 1.0 OK ; else echo release 1.0 BAD ; fi
+if [ -f new.data ] ; then echo f_4 OK ; else echo f_4 BAD ; fi
