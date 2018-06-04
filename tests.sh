584279494f9f74f86b6fabc050d6852b0aff7278
gilhad <gilhad@seznam.cz>
2018-06-04 21:45:27 +0200

    Closed due MS acquisition, find me at https://divnoblog.wordpress.com/

--- tests.sh
+++
@@ -1,5 +0,0 @@
-if [ -f new.data ] ; then echo f_1 OK ; else echo f_1 BAD ; fi
-if [ -f new.data ] ; then echo f_2 OK ; else echo f_2 BAD ; fi
-if [ -f version.txt ] ; then echo release 1.0 OK ; else echo release 1.0 BAD ; fi
-if [ -f neco.txt ] ; then echo f_3 OK ; else echo f_3 BAD ; fi
-if [ -f new.data ] ; then echo f_4 OK ; else echo f_4 BAD ; fi
